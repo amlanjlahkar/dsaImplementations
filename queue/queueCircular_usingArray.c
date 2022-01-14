901de537dcd3c5c611359277816ba63530bffe36
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-01-14 13:14:01 +0530

    ip.misc.qCirc
    removed/improved unnecessary lines

--- queue/queueCircular_usingArray.c
+++ queue/queueCircular_usingArray.c
@@ -2,16 +2,10 @@
  * that is the first item to enter the queue
  * is the first item to be removed from the queue -
  * containing integers, using array.
- *
- * AUTHOR: Amlanjyoti Lahkar
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-
-// for colored printing of critical warnings/errors
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
 
 #define QUEUESIZE 5
 typedef struct {
@@ -23,8 +17,7 @@ queuerec q = { .frontindex = -1, .rearindex = -1 };
 // variables to test for empty queue
 int TRUE = 1, FALSE = 0, *emptindi;
 
-
-// queue function prototypes
+// queue functions
 int isEmpty(queuerec *);
 int isFull(queuerec *);
 void enQueue(queuerec *, int);
@@ -34,75 +27,63 @@ int queueTotal(queuerec *);
 // miscellaneous
 void warn(int *);
 
-
 int main(void) {
     int item = 0, input = 0, enqueued = 0, dequeued = 0;
     // point emptindi to true at the beginning
     emptindi = &TRUE;
 
-    printf("\nThe queue can contain atmost %d items and is currently empty.\n"
-    "Below are some operations available to perform on it.\n", QUEUESIZE);
-
-    // printig the menu infinitely until the specified condition is met
+    printf("\n(queue capacity = %d)\n", QUEUESIZE);
     while(1) {
-        puts("\n--------------------------------------------------");
-        puts("\n1) isEmpty (indicates either TRUE or FALSE)\n"
-        "2) enqueue (insert a new item at the end of the queue)\n"
-        "3) dequeue (remove an item from the front of the queue)\n"
-        "4) peek (get an overview of current items in the queue)\n"
-        "5) exit\n");
-        puts("--------------------------------------------------");
+        puts("\n1) insert a new item to the queue"
+             "\n2) remove an item from the queue"
+             "\n3) print out the queue items"
+             "\n4) exit\n");
 
-        printf("\nEnter your input(1,2,3,4 or 5): ");
+        printf("\nenter your input: ");
         warn(&input);
-        if (input < 1 || input > 5) {
-            puts(ANSI_COLOR_RED "\nPlease provide a valid input." ANSI_COLOR_RESET);
+        if (input < 1 || input > 4) {
+            puts("\nplease provide a valid input.");
             continue;
-        } else if (input == 5) {
-            puts("\nProgram terminated.\n");
+        } else if (input == 4) {
+            puts("\nterminated.\n");
             break;
         }
 
         switch(input) {
-            // is empty
-            case 1 :
-                puts((isEmpty(&q)) ? "\nTRUE" : "\nFALSE");
-                break;
-            // enqueue
-            case 2 :
+            case 1 : {
                 if (isFull(&q)) {
-                    puts(ANSI_COLOR_RED "\nQueue is already full!" ANSI_COLOR_RESET);
+                    puts("queue is already full!");
                     break;
                 }
-                printf("\nEnter an item(integer): ");
+                printf("\nenter a value(integer) for the item: ");
                 warn(&enqueued);
                 enQueue(&q, enqueued);
                 if (isFull(&q))
-                    printf("\nThe last remaining place in the queue\nhas been filled up"
-                    " with the new item \'%d\'.\nThe queue is now full.\n", enqueued);
+                    printf("item has been inserted at the end of the queue.\n"
+                    "the queue is now full!\n");
                 else
-                    printf("\nA new item \'%d\' has been inserted at the rear of the queue.\n"
-                    "It now contains %d item(s).\n", enqueued, queueTotal(&q));
+                    printf("item has been inserted at the end of the queue.\n"
+                    "(current number of item(s) remaining in the queue = %d)\n", queueTotal(&q));
                 break;
-            // dequeue
-            case 3 :
+            }
+            case 2 : {
                 if ((dequeued = deQueue(&q))) {
                     if (isEmpty(&q)) {
-                        printf("\nThe last item \'%d\' of the queue has been removed.\n"
-                        "The queue is now empty!\n", dequeued);
+                        printf("item \'%d\' from the front of the queue has been removed.\n"
+                        "the queue is now empty!\n", dequeued);
                     }
                     else
-                        printf("\nThe item \'%d\' from the front of the queue has been removed.\n"
-                        "It now contains %d item(s).\n", dequeued, queueTotal(&q));
+                        printf("item \'%d\' from the front of the queue has been removed.\n"
+                        "(number of item(s) remaining in the queue = %d)\n", dequeued, queueTotal(&q));
                     break;
                 }
                 break;
-            // peek
-            case 4 :
+            }
+            case 3 : {
                 if (isEmpty(&q))
-                    puts("\nThe queue is empty!");
+                    puts("\nqueue is empty!");
                 else {
-                    puts("\nThe item(s) currently in the queue are:\n");
+                    printf("\n");
                         for (int i = 0; i < QUEUESIZE; i++) {
                             if (i == q.frontindex)
                                 printf("%7d\t<--- front\n", q.members[i]);
@@ -113,12 +94,12 @@ int main(void) {
                         }
                 }
                 break;
+            }
         }
     }
 }
 
 
-// function definitions
 int isEmpty(queuerec *ptr) {
     return((ptr->frontindex == -1 || *emptindi == TRUE) ? 1 : 0);
 }
@@ -131,11 +112,10 @@ int isFull(queuerec *ptr) {
     return((index_after_rear == ptr->frontindex) ? 1 : 0);
 }
 
-// shift rearindex to one index ahead when "inserting" item
 void enQueue(queuerec *ptr, int rearelement) {
     if (isFull(ptr)) {
-        puts(ANSI_COLOR_RED "\nAttempting to add item to a non-empty queue"
-        " will cause overflow!" ANSI_COLOR_RESET);
+        puts("\nAttempting to add item to a non-empty queue"
+        " will cause overflow!");
         return;
     }
     else if (isEmpty(ptr))
@@ -146,11 +126,10 @@ void enQueue(queuerec *ptr, int rearelement) {
     ptr->members[ptr->rearindex] = rearelement;
 }
 
-// shift frontindex to one index ahead when "removing" item
 int deQueue(queuerec *ptr) {
     if(isEmpty(ptr)) {
-        puts(ANSI_COLOR_RED "\nAttempting to access item from an empty queue"
-        " will cause underflow!" ANSI_COLOR_RESET);
+        puts("\nAttempting to access item from an empty queue"
+        " will cause underflow!");
         return 0;
     }
     // store the current element at frontinex in a temporary var for returning
@@ -165,7 +144,6 @@ int deQueue(queuerec *ptr) {
     return prev_item;
 }
 
-// return the number of total items in the queue
 int queueTotal(queuerec *ptr) {
     if (isEmpty(ptr)) {
         int zero = 0;
@@ -185,7 +163,7 @@ int queueTotal(queuerec *ptr) {
 // warn() ensures successful reading of single integer from stdin
 void warn(int *a) {
     if (scanf(" %d", a) != 1) {
-        puts(ANSI_COLOR_RED "\nError reading input\n" ANSI_COLOR_RESET
+        puts("\nError reading input\n"
         "(please re-run the compiled program)\n");
         exit(1);
     }
