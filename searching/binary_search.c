12e2889c33abc7a69cdbd81553636d89a5f5298c
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2022-04-08 21:03:51 +0530

    add(searching)

---
+++ searching/binary_search.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int binary_search(int array[], int lb, int ub, int element) {
+  if (lb <= ub) {
+    int mid = (lb + ub) / 2;
+    if (array[mid] == element)
+      return mid;
+    else if (array[mid] > element)
+      return binary_search(array, lb, mid - 1, element);
+    else
+      return binary_search(array, mid + 1, ub, element);
+  }
+  return -1;
+}
+
+int main(void) {
+  int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int element;
+  printf("Enter element to search for in the array: ");
+  scanf("%d", &element);
+
+  int index = binary_search(array, 0, 9, element);
+  if (index == -1)
+    printf("The element %d is not present in the array.\n", element);
+  else
+    printf("The element %d is present at index %d in the array.\n", element,
+           index);
+  return 0;
+}
---
+++ searching/linear_search.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int linear_search(int array[], int array_size, int element) {
+  int index = 0;
+  int check = 0;
+  for (int i = 0; i < array_size; i++) {
+    if (array[i] == element) {
+      check++;
+      break;
+    } else
+      index++;
+  }
+  return ((check == 0) ? -1 : index);
+}
+
+int main(void) {
+  int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int element;
+  printf("Enter element to search for in the array: ");
+  scanf("%d", &element);
+
+  int index = linear_search(array, 10, element);
+  if (index == -1)
+    printf("The element %d is not present in the array.\n", element);
+  else
+    printf("The element %d is present at index %d in the array.\n", element,
+           index);
+  return 0;
+}
